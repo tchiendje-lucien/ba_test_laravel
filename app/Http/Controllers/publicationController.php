f4497f54969d39bb325b9513596f0096c866ad61
TCHIENDJE Lucien <tchiendjelucien@gmail.com>
2021-12-29 17:00:01 +0100

    modification last insert id du produit

--- app/Http/Controllers/publicationController.php
+++ app/Http/Controllers/publicationController.php
@@ -123,12 +123,12 @@ class publicationController extends Controller
                 ]
             );
             if ($insert_prod) {
-                $id_prod = DB::select("SELECT currval(pg_get_serial_sequence('produits','ID_PRODUIT'));");
-                echo $id_prod;
+                $id_prod = DB::select("SELECT currval(pg_get_serial_sequence('produits','ID_PRODUIT'));")[0];
+                dd($id_prod);
                 $insert_pub = DB::table('publications')->insert(
                     [
                         "ID_USER" => session::get('id_user'),
-                        "ID_PRODUIT" => $id_prod[0],
+                        "ID_PRODUIT" => $id_prod,
                         "ETAT_PUB" => 1,
                         "DATE_PUB" => Carbon::now(),
                         "DATE_MODIF_PUB" => Carbon::now()
